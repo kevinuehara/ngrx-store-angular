24c31359908fd546427ce4e66379df2de717f083
kevin uehara <uehara.kevin@gmail.com>
2020-04-17 18:34:32 -0300

    feat: implements actions, reducers, effects and selector for toppings

--- src/products/store/effects/index.ts
+++ src/products/store/effects/index.ts
@@ -1,5 +1,7 @@
+import { ToppingsEffects } from './toppings.effect';
 import { PizzasEffects } from './pizzas.effect';
 
-export const effects: any[] = [PizzasEffects];
+export const effects: any[] = [PizzasEffects, ToppingsEffects];
 
 export * from './pizzas.effect';
+export * from './toppings.effect';
--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,40 +1,15 @@
-import {
-  ActionReducerMap,
-  createSelector,
-  createFeatureSelector,
-} from "@ngrx/store";
+import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
 import * as fromPizzas from "./pizzas.reducer";
+import * as fromToppings from "./toppings.reducer";
 
 export interface ProductState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 export const reducers: ActionReducerMap<ProductState> = {
   pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer,
 };
 
 export const getProductsState = createFeatureSelector<ProductState>("products");
-
-// pizzas state
-export const getPizzaState = createSelector(
-  getProductsState,
-  (state: ProductState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities
-);
-
-export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
-  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
-});
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
-);
